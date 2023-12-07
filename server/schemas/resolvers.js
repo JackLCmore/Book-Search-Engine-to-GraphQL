6726c2f808b421868ebe28291a35e31aa1e5d5fb
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-06 17:57:57 -0800

    completed resolvers for server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,6 +12,48 @@ const resolvers = {
     },
 
     Mutation: {
-        
+        addUser: async (parent, {username, email, password})=>{
+            const user = await User.create({username, email, password}):
+            const token = signToken(user);
+
+            return {token, user};
+        },
+        login: async (parent, {email, password})=>{
+            const user = await User.findOne({email});
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return {token, user};
+        },
+        saveBook: async (parent, {authors,description, title, bookId, image, link}, context)=>{
+            if(context.user){
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addtoset: {authors:authors, description:description, title:title, bookId:bookId, image:image, link:link } },
+                    { new: true }
+                )
+            }
+        },
+        removeBook: async (parent, {bookId}, context)=>{
+            if(context.user){
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: {bookId:bookId} },
+                    { new: true }
+                );
+            }
+            throw AuthenticationError;
+        }
     }
-}
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,8 +29,8 @@ const typeDefs = `
         addUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
 
-        saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String!, link: String!): User
-        removeBook(bookId: String):User
+        saveBook(authors: [String]!, description: String!, title: String!, bookId: String!, image: String!, link: String!): User
+        removeBook(bookId: String!):User
     }
 `;
 
